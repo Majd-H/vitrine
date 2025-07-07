ac015513ff128c282f6c47023186120d0caf24d5
Majd-H <majdhoche@yahoo.com>
2025-07-07 23:04:05 +0200

    debug non terminé compnents

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { Component } from '@angular/core';
 
 import { routes } from './app.routes';
+
+import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
-  ]
+  ],
+  imports: [Header, Footer]
 };
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.config';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { Header } from './app/shared/header/header';
+import { Footer } from './app/shared/footer/footer';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig);
+
